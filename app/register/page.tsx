d8f04f5db92280405c6ccfd5497b2e2a75c4ded9
3bdullah <abdullahhisham977@gmail.com>
2024-05-03 19:49:43 +0300

    adding validation

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,26 +4,53 @@ import "../../style/register.scss";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type authData = {
+  name: string;
+  email: string;
+  password: string;
+  loginEmail: string;
+  loginPass: string;
+};
 
 export default function Register() {
   const [toggle, setToggle] = useState(false);
+  const form = useForm<authData>();
+  const { register, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = (data: authData) => {
+    // DB !!!!
+  };
 
   return (
     <>
       <div className={`container rounded-lg ${toggle && "active"}`}>
         <div className="form-container sign-in-container">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)} noValidate>
             <h1 className="font-extrabold text-3xl">Sign in</h1>
             <Input
               className="bg-[#eee] rounded-none py-5"
-              placeholder="Email"
+              placeholder="admin@example.com"
               type="email"
+              {...register("loginEmail", {
+                pattern: {
+                  value:
+                    /^[a-zA-Z0-9.!#$%&'*+/=?^_^{1}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: "invalid email address",
+                },
+                required: "email is required",
+              })}
             />
+            <p className="text-red-500">{errors.loginEmail?.message}</p>
             <Input
               className="bg-[#eee] rounded-none py-5"
               placeholder="Password"
               type="password"
+              {...register("loginPass", { required: "password is required" })}
             />
+            <p className="text-red-500">{errors.loginPass?.message}</p>
             <a href="#" className="py-3">
               Forget your password?
             </a>
@@ -35,20 +62,36 @@ export default function Register() {
           </form>
         </div>
         <div className="form-container sign-up-container">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)} noValidate>
             <h1 className="font-extrabold text-3xl">Sign UP</h1>
             <span className="text-sm">or use your email for registration</span>
-            <Input className="bg-[#eee] rounded-none py-5" placeholder="Name" />
             <Input
               className="bg-[#eee] rounded-none py-5"
-              placeholder="Email"
-              type="email"
+              placeholder="Name"
+              {...register("name", { required: "name is required" })}
             />
+            <p className="text-red-500">{errors.name?.message}</p>
+            <Input
+              className="bg-[#eee] rounded-none py-5"
+              placeholder="admin@example.com"
+              type="email"
+              {...register("email", {
+                pattern: {
+                  value:
+                    /^[a-zA-Z0-9.!#$%&'*+/=?^_^{1}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: "invalid email address",
+                },
+                required: "email is required",
+              })}
+            />
+            <p className="text-red-500">{errors.email?.message}</p>
             <Input
               className="bg-[#eee] rounded-none py-5"
               placeholder="Password"
               type="password"
+              {...register("password", { required: "password is required" })}
             />
+            <p className="text-red-500">{errors.password?.message}</p>
             <Button
               variant="outline"
               className="rounded-xl px-10 bg-[#023e8a] text-white">
