0d2a584b7a609931249b04da3ccc71b3cc65be5c
Ahmed <ahmedelsharawy117@gmail.com>
2024-05-07 05:28:47 +0300

    added api.tsx

---
+++ app/api.tsx
@@ -0,0 +1,7 @@
+export const getBooks = "http://localhost:8080/api/v1/books";
+
+export const getBookById = "http://localhost:8080/api/v1/books/"; // +id
+
+export const getOrders = "http://localhost:8080/api/v1/orders";
+
+export const createOrder = "http://localhost:8080/api/v1/orders"; // add the body
